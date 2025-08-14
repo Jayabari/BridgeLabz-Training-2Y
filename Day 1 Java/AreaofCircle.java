787db2594114fb751ee663230f09e73d72f9167d
Jaya <jayabari849@gmail.com>
2025-08-14 11:59:06 +0530

    [Jaya] added practice problem

---
+++ Day 1 Java/AreaofCircle.java
@@ -0,0 +1,7 @@
+public class AreaofCircle {
+    public static void main(String[] args) {
+        float radius = 4;
+        double Area = Math.PI * Math.pow(radius,2);
+        System.out.println(Area);
+    }
+}
---
+++ Day 1 Java/CalculateSI.java
@@ -0,0 +1,9 @@
+public class CalculateSI {
+    public static void main(String[] args) {
+        float principal = 100;
+        float rate = 15;
+        int time = 3;
+        double SI = (principal * rate * time)/100 ;
+        System.out.println(SI);
+    }
+}
